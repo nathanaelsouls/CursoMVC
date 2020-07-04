96cd64c98a3e69cf9964e22b92cd4346a3df906b
Nathanael F Santana <nathanaelotaku28@gmail.com>
2020-07-04 15:54:17 -0300

    Add project files.

---
+++ CursoMVC/Models/Context.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVC.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=LAPTOP-NATHANAE\SQLEXPRESS;Database=Cursomvc;Integrated Security=True");
+        }
+
+        public virtual void SetModified(object entity)
+        {
+            Entry(entity).State = EntityState.Modified;
+        }
+    }
+}
